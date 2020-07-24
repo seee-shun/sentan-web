65b3dd344efabd9a4bebf4f92dc3a0cec38a5de4
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2020-07-24 23:27:55 +0900

    テキストの訂正

--- components/project.vue
+++ components/project.vue
@@ -4,37 +4,32 @@
       <h1>プラットフォームサービスのエコシステム形成に関する実践的研究</h1>
       <v-card class="mx-auto project-card">
         <v-card-text>
-          <p class="display-1 text--primary">
-            Missoin.
+          <p class="display-2 indigo--text font-weight-bold mt-20">
+            Project.
           </p>
-          <p>adjective</p>
-          <div class="text--primary">
-            well meaning and kindly.<br />
-            "a benevolent smile"
-          </div>
+          <p class="display-1 text--primary">
+            プラットフォームサービスの実習環境を構築する
+          </p>
+          <p class="text--primary">
+            モバイルキャンパスアプリ「パンプラージュ」を開発・運用することで、プラットフォームサービスの実習環境を構築する。その実践的取り組みの中でチキンエッグ問題（コンテンツ
+            制作・システム開発・ユーザ獲得） の解消方法を模索する。
+          </p>
         </v-card-text>
-        <v-card-actions>
-          <v-btn text color="accent-4">
-            Learn More
-          </v-btn>
-        </v-card-actions>
       </v-card>
       <v-card class="mx-auto project-card">
         <v-card-text>
-          <p class="display-1 text--primary">
-            Vision.
+          <p class="display-2 indigo--text font-weight-bold mt-20">
+            Service.
           </p>
-          <p>adjective</p>
-          <div class="text--primary">
-            well meaning and kindly.<br />
-            "a benevolent smile"
-          </div>
+          <p class="text--primary">
+            浜松キャンパス生のためのスマホアプリ「パンプラージュ」
+          </p>
+          <p class="text--primary">
+            パンプラージュで、できること<br />
+            ①静大生への質問機能 ②時間割の管理 ③イベントの共有
+            ④学生によるコンテンツの閲覧 ⑤部活・サークル情報の閲覧
+          </p>
         </v-card-text>
-        <v-card-actions>
-          <v-btn text color="accent-4">
-            Learn More
-          </v-btn>
-        </v-card-actions>
       </v-card>
     </div>
   </div>
@@ -43,6 +38,5 @@
 .project-card {
   margin: 50px;
   margin-left: 100px;
-  text-align: center;
 }
 </style>
