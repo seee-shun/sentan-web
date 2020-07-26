ee92977ae6e93e28457223afbfb707825aa1fc62
Hiroaki Tsurusaki <tsurusaki.hiroaki.19@shizuoka.ac.jp>
2020-07-26 20:23:14 +0900

    footerのUIを変更した

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <v-footer>
-    <h1>Footer</h1>
+  <v-footer class="pa-3">
+    <v-col class="text-center" cols="12">
+      <div>&copy; {{ new Date().getFullYear() }} - sentan</div>
+    </v-col>
   </v-footer>
 </template>
